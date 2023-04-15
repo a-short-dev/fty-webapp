7e5cc9ae13f344ecb1650f0775b1431277decdc8
Leke Abiodun <lakessyde1@gmail.com>
2023-04-15 01:29:39 +0100

    ui-changes

--- app/user/components/Header.tsx
+++ app/user/components/Header.tsx
@@ -1,33 +1,19 @@
 import Image from "next/image";
 import React from "react";
-import {
-  AiFillNotification,
-  AiOutlineMenu,
-  AiOutlineNotification,
-} from "react-icons/ai";
+import { AiOutlineBell, AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div className="fixed transition top-0 w-full bg-white shadow">
       <div className="flex items-center justify-between px-5 py-3">
         <AiOutlineMenu size={24} className="rounded-md font-medium" />
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <div>
-            <AiOutlineNotification
-              size={24}
-              className="rounded-md fontme
-            "
-            />
+            <AiOutlineBell size={24} className="rounded-md font-medium" />
           </div>
-
-          <Image
-            src="/next.svg"
-            width={50}
-            height={40}
-            alt=""
-            className="rounded-full p-2"
-            priority
-          />
+          <div className=" rounded-full w-10 h-10 border px-2 relative">
+            <Image src="/next.svg" fill={true} alt="" priority />
+          </div>
         </div>
       </div>
     </div>
