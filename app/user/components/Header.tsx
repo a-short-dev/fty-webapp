ca0beaf4590543ea4cfb8c314c1963a8a56a6f3f
Leke Abiodun <lakessyde1@gmail.com>
2023-04-15 05:14:13 +0100

    ui changes

--- app/user/components/Header.tsx
+++ app/user/components/Header.tsx
@@ -9,9 +9,9 @@ interface HeaderProps {
 
 const Header = ({ firstName, avatar }: HeaderProps) => {
   return (
-    <div className="fixed transition top-0 w-full bg-white shadow">
+    <div className="fixed z-40 transition top-0 w-full bg-white shadow">
       <div className="flex items-center justify-between px-5 py-3">
-        <h3 className="font-normal text-lg">Hi,{firstName || "User"}</h3>
+        <h3 className="font-medium text-lg">Hi,{firstName || "User"}</h3>
         <div className="flex items-center gap-2">
           <div className="relative">
             <AiOutlineBell size={24} className="rounded-md font-medium" />
